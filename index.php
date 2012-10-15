d1ac1189db98b6c2a981f4fecf2aa926cbd6f272
Таня <@derek-faster.mshome.net>
2012-10-16 00:48:28 +0300

    первая фиксация инициализирован git созданы файлы index.php, style.css создана папка img и добавлены фоновые изображения для шапки и мидла в index.php написан базовый каркас, и левый сайдбар (главное меню)

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>L2 сервер - Главная</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+      <div id="wrapper">
+          <!--Заголовок страницы-->
+          <div id="header">
+              
+          </div>
+         
+          <div id="middle">
+           <!--Левое меню-->
+           <div id="l_sidebar">
+               <ul class="common_menu">
+                   <li>
+                       <a href="">Главная</a> 
+                   </li>
+                   <li>
+                       <a href="">О сервере</a>
+                   </li>
+                   <li>
+                       <a href="">Регистрация</a>
+                   </li>
+                   <li>
+                       <a href="">Статистика</a>
+                   </li>
+                   <li>
+                       <a href="">Пожертвования</a>
+                   </li>
+                   <li>
+                       <a href="">Подключения</a>
+                   </li>
+                   <li>
+                       <a href="">Правила</a>
+                   </li>
+                   <li>
+                       <a href="">Файлы</a>
+                   </li>
+                   <li>
+                       <a href="">Форум</a>
+                   </li>
+                   <li>
+                       <a href="">Администрация</a>
+                   </li>
+                   <li>
+                       <a href="">Бонус</a>
+                   </li>
+               </ul>
+           </div>  
+           <!--Содержимое-->
+           <div id="content">
+               
+           </div>
+           <!--Правое меню-->
+           <div id="r_sidebar">
+               
+           </div>
+       </div>
+       <!--Подвал страницы-->
+       <div id="footer">
+          
+       </div>
+      </div>
+     
+  </body>
+</html>
